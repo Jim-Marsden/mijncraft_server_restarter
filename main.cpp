7ffe90329c8384aa2998c787102467299c016695
Jim <jamespmarsden@hotmail.com>
2021-01-23 20:54:25 -0800

    basic start on core main loop

--- main.cpp
+++ main.cpp
@@ -3,12 +3,19 @@
 #include <cstdio>
 #include <string>
 
-int main() {
-  std::string x{"java -server"};
-  auto File = popen(x.c_str(), "rw");
-  if(File){
+#include "cmd_parser.hpp"
 
-  }
+int main(int argc, char **argv) {
+  auto start_data = mijn::parser(argc, argv);
+  do {
+    auto File = popen(start_data.start_data.c_str(), "rw");
+    if(File){
+
+    } else {std::cerr << "Failed to open: " << start_data.start_data << '\n'; std::exit(EXIT_FAILURE);}
+
+    pclose(File);
+  } while (start_data.do_loop);
+
   std::cout << "Hello, World!" << std::endl;
   return 0;
 }
